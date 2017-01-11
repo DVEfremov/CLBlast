0f0ac45355b6f3f4a2bea47ceff09a271209f0c7
Daniil Efremov <daniil.efremov@xored.com>
2017-01-11 14:58:19 +0700

    debug

--- src/android_patch.h
+++ src/android_patch.h
@@ -2,22 +2,34 @@
 // Created by daniil on 1/11/17.
 //
 
-#ifndef CLBLAST_ANDROID_PATCH_H
-#define CLBLAST_ANDROID_PATCH_H
+//#ifndef CLBLAST_ANDROID_PATCH_H
+//#define CLBLAST_ANDROID_PATCH_H
+//
+//
+//#include <string>
+//#include <sstream>
+//
+//
+//    template < typename T > std::string to_string( const T& n )
+//    {
+//        std::ostringstream stm ;
+//        stm << n ;
+//        return stm.str() ;
+//    }
+//
+//
+//#endif //CLBLAST_ANDROID_PATCH_H
 
 
-#include <string>
-#include <sstream>
+#include<sstream>
+template <typename T>
+std::string to_string(T value) {
+    //create an output string stream
+    std::ostringstream os ;
 
-namespace patch
-{
-    template < typename T > std::string to_string( const T& n )
-    {
-        std::ostringstream stm ;
-        stm << n ;
-        return stm.str() ;
-    }
+    //throw the value into the string stream
+    os << value ;
+
+    //convert the string stream into a string and return
+    return os.str() ;
 }
-
-#endif //CLBLAST_ANDROID_PATCH_H
-
